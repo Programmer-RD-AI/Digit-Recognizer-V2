e58877224a834e09f08cbfdea15c8611917af4e4
Programmer-RD-AI <go2ranuga@gmail.com>
2023-07-09 21:17:07 +0530

    Update metrics.py
    Co-Authored-By: Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>

--- ML/metrics.py
+++ ML/metrics.py
@@ -18,28 +18,26 @@ class Metrics:
 
     def accuracy(self):
         tot = 0
+        cor = 0
         for X, y in self.dataloader:
             preds = torch.argmax(torch.softmax(self.model(X.to(self.device)), dim=1), dim=1)
-            tot = 0
-            cor = 0
+
             for pred, tr in zip(preds, y):
                 if pred == tr:
                     # print(pred, tr, len(self.dataloader))
                     cor += 1
                 tot += 1
-            tot += cor / tot
-        return (tot / len(self.dataloader)) * 100
+        return (cor / tot) * 100
 
     def precision(
         self,
     ):
-        tot = 0
+        tp = 0
+        fp = 0
         for X, y in self.dataloader:
             preds = torch.argmax(torch.softmax(self.model(X.to(self.device)), dim=1), dim=1)
-            tot_prec = 0
             for clz in self.classes:
-                tp = 0
-                fp = 0
+
                 for pred, tr in zip(preds.to(self.device), y.to(self.device)):
                     if pred == clz and tr == pred:
                         # print(pred, clz, tr)
@@ -48,9 +46,4 @@ class Metrics:
                         # print(pred, clz, tr)
                         fp += 1
                 # print(tp, fp)
-                try:
-                    tot_prec += tp / (tp + fp)
-                except:
-                    tot_prec += 0
-            tot += tot_prec / len(self.classes)
-        return tot / len(self.dataloader)
+        return tp / (tp + fp)
