f7b6b21758338543515367139891fd6fe10b5512
Programmer-RD-AI <go2ranuga@gmail.com>
2023-07-09 18:59:19 +0530

    Update linear.py
    Co-Authored-By: Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>

--- ML/modelling/linear.py
+++ ML/modelling/linear.py
@@ -11,44 +11,61 @@ class LinearModel(nn.Module):
             nn.Linear(in_size, hidden_unis),
             nn.BatchNorm1d(hidden_unis, affine=affine),
             nn.Dropout(),
+            nn.ReLU(),
         )
         self.linblo2 = nn.Sequential(
             nn.Linear(hidden_unis, hidden_unis * 2),
             nn.BatchNorm1d(hidden_unis * 2, affine=affine),
+            nn.Dropout(),
+            nn.ReLU(),
         )
         self.linblo3 = nn.Sequential(
             nn.Linear(hidden_unis * 2, hidden_unis * 3),
             nn.BatchNorm1d(hidden_unis * 3, affine=affine),
+            nn.Dropout(),
+            nn.ReLU(),
         )
         self.linblo4 = nn.Sequential(
             nn.Linear(hidden_unis * 3, hidden_unis * 4),
             nn.BatchNorm1d(hidden_unis * 4, affine=affine),
+            nn.Dropout(),
+            nn.ReLU(),
         )
         self.linblo5 = nn.Sequential(
             nn.Linear(hidden_unis * 4, hidden_unis * 5),
             nn.BatchNorm1d(hidden_unis * 5, affine=affine),
             nn.Dropout(),
+            nn.ReLU(),
         )
         self.linblo6 = nn.Sequential(
             nn.Linear(hidden_unis * 5, hidden_unis * 6),
             nn.BatchNorm1d(hidden_unis * 6, affine=affine),
+            nn.Dropout(),
+            nn.ReLU(),
         )
         self.linblo7 = nn.Sequential(
             nn.Linear(hidden_unis * 6, hidden_unis * 7),
             nn.BatchNorm1d(hidden_unis * 7, affine=affine),
+            nn.Dropout(),
+            nn.ReLU(),
         )
         self.linblo8 = nn.Sequential(
             nn.Linear(hidden_unis * 7, hidden_unis * 8),
             nn.BatchNorm1d(hidden_unis * 8, affine=affine),
+            nn.Dropout(),
+            nn.ReLU(),
         )
         self.linblo9 = nn.Sequential(
             nn.Linear(hidden_unis * 8, hidden_unis * 9),
             nn.BatchNorm1d(hidden_unis * 9, affine=affine),
+            nn.Dropout(),
+            nn.ReLU(),
         )
         self.linblo10 = nn.Sequential(
             nn.Linear(hidden_unis * 9, hidden_unis * 8),
             nn.BatchNorm1d(hidden_unis * 8, affine=affine),
             nn.Dropout(),
+            nn.ReLU(),
         )
         self.out = nn.Linear(hidden_unis * 8, out_size)
 
