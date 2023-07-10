63c30d7b8acd9da20e9a1ba9aca56ddbfe2c8377
Programmer-RD-AI <go2ranuga@gmail.com>
2023-07-10 22:00:40 +0530

    Create alert.py
    Co-Authored-By: Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>

---
+++ ML/helper/alert.py
@@ -0,0 +1,18 @@
+from ML import *
+
+
+class Alert:
+    def __init__(self, results, past_results):
+        self.results = results
+        self.past_results = past_results
+
+    def alert(self):
+        msges = ""
+        for key in self.results.keys():
+            v = self.results[key]
+            v_p = self.past_results[key]
+            if "loss" in key.split() and v > v_p:
+                msges += f"{key} has lowered"
+            else:
+                msges += f"{key} has increased"
+        return msges
