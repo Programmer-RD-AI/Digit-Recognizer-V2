787e8585124cc963df4908f958b62ba3a3634efc
Programmer-RD-AI <go2ranuga@gmail.com>
2023-07-10 22:00:44 +0530

    Create normalizer.py
    Co-Authored-By: Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>

---
+++ ML/helper/normalizer.py
@@ -0,0 +1,31 @@
+from ML import *
+
+
+class Normalizer:
+    def __init__(self, path, label_col):
+        self.data = pd.read_csv(path).drop(label_col, axis=1)
+        self.no = len(self.data)
+        self.create_long_list()
+
+    def std(self):
+        """
+        \sigma={\sqrt {\frac {\sum(x_{i}-{\mu})^{2}}{N}}}
+        """
+        # mean = sum(self.tot_imgs) / len(self.tot_imgs)  # mean
+        # var = sum(pow(x - mean, 2) for x in self.tot_imgs) / len(self.tot_imgs)  # variance
+        # std = math.sqrt(var)
+        # print(std, np.std(np.array(self.tot_imgs)))
+        return np.std(np.array(self.tot_imgs))
+
+    def create_long_list(self):
+        self.tot_imgs = []
+        for i in range(self.no):
+            self.tot_imgs.append(np.array(self.data.iloc[i].tolist()) / 255)
+        self.tot_imgs = torch.tensor(self.tot_imgs).squeeze().view(self.no * 784).float()
+        return self.tot_imgs
+
+    def mean(self) -> float:
+        tot = 0
+        for i in tqdm(range(len(self.data))):
+            tot += (np.array(self.data.iloc[i].tolist()) / 255).mean()
+        return float(tot / self.no)
