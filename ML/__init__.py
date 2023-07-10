6f6a60b0a4eb04f515b2aaa553cbd86ae67e291d
Programmer-RD-AI <go2ranuga@gmail.com>
2023-07-10 22:00:33 +0530

    Update __init__.py
    Co-Authored-By: Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>

--- ML/__init__.py
+++ ML/__init__.py
@@ -17,13 +17,18 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from wandb import AlertLevel
+import math
 
+os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
+os.environ["WANDB_SILENT"] = "true"
 PROJECT_NAME = "Digit Recognizer V2"
-device = "cuda" if torch.cuda.is_available() else "cpu"
+# device = torch.cuda.set_device(0)
+device = torch.device("cuda")
+torch.cuda.empty_cache()
 torch.manual_seed(42)
 np.random.seed(42)
 torch.cuda.manual_seed(42)
 from ML.metrics import *
-from ML.helper_funcs import *
+from ML.helper import *
 from ML.data_loader import *
 from ML.modelling import *
